d0288bdfd9af9d9bd6167bc2b8a39bf78b56849a
Abdulla Al Shahid <shahidabdulla.aas@gmail.com>
2021-10-29 01:11:28 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -24,6 +24,47 @@ client.connect((err) => {
   } else {
     console.error("[*] Database Connection Failed.");
   }
+
+  async function run() {
+    try {
+      await client.connect();
+      const database = client.db("carMechanic");
+      const haiku = database.collection("services");
+      //GET API
+      app.get("/services", async (req, res) => {
+        const cursor = haiku.find({});
+        const users = await cursor.toArray();
+        res.send(users);
+      });
+
+      //GET SINGLE SERVICE
+      app.get("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log("[*] Getting single service id", id);
+        const query = { _id: ObjectId(id) };
+        const service = await haiku.findOne(query);
+        res.json(service);
+      });
+      //POST API
+      app.post("/services", async (req, res) => {
+        console.log("[*] Service uploaded to database");
+        const service = req.body;
+        const result = await haiku.insertOne(service);
+        res.json(result);
+      });
+      //DELETE API
+      app.delete("/dbuser/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await haiku.deleteOne(query);
+        console.log("deleteing user with id", result);
+        res.json(result);
+      });
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
 });
 
 // Server Status
