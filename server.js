6c6e2bf116ee2083005ad4437e5b4aebc60e1716
Abdulla Al Shahid <shahidabdulla.aas@gmail.com>
2021-10-31 22:08:52 +0600

    final-commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
-const port = process.env.PORT || 4356;
+const port = 1234;
 const app = express();
 require("dotenv").config();
 
@@ -11,6 +11,11 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// SERVER STATUS
+app.get("/", (req, res) => {
+  res.status(200).send("Server Running [OK]");
+});
+
 //MongoDb Connection and Setup API
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_TARGET_DB}?retryWrites=true&w=majority`;
@@ -18,6 +23,7 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 client.connect((err) => {
   if (err === undefined) {
     console.log("[*] Database Connected Successfully.");
@@ -28,13 +34,32 @@ client.connect((err) => {
   async function run() {
     try {
       await client.connect();
-      const database = client.db("carMechanic");
+      const database = client.db("travel");
       const haiku = database.collection("services");
-      //GET API
+      const database2 = client.db("reviews");
+      const haiku2 = database2.collection("rview");
+      const database3 = client.db("Orders");
+      const haiku3 = database3.collection("orderdata");
+
+      //GET API FOR SERVICES
       app.get("/services", async (req, res) => {
-        const cursor = haiku.find({});
+        const cursor = haiku.find();
         const users = await cursor.toArray();
         res.send(users);
+      });
+
+      //GET API FOR REVIEWS
+      app.get("/reviews", async (req, res) => {
+        const cursor2 = haiku2.find({});
+        const users2 = await cursor2.toArray();
+        res.send(users2);
+      });
+
+      //GET API FOR ORDERINFO
+      app.get("/placeorder", async (req, res) => {
+        const cursor3 = haiku3.find({});
+        const users3 = await cursor3.toArray();
+        res.send(users3);
       });
 
       //GET SINGLE SERVICE
@@ -45,18 +70,37 @@ client.connect((err) => {
         const service = await haiku.findOne(query);
         res.json(service);
       });
-      //POST API
-      app.post("/services", async (req, res) => {
-        console.log("[*] Service uploaded to database");
-        const service = req.body;
-        const result = await haiku.insertOne(service);
-        res.json(result);
+
+      //GET SINGLE ORDER INFO
+      app.get("/placeorder/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log("[*] Getting single service id", id);
+        const query = { _id: ObjectId(id) };
+        const orderInfo = await haiku3.findOne(query);
+        res.json(orderInfo);
       });
+
+      //POST API FOR ORDER INFO
+      app.post("/placeorder", async (req, res) => {
+        const orderInfo = req.body;
+        const result = await haiku3.insertOne(orderInfo);
+        res.json(result);
+        console.log("[*] Service uploaded to database");
+      });
+
+      //POST API FOR SERVICES
+      app.post("/services", async (req, res) => {
+        const serviceInfo = req.body;
+        const result = await haiku.insertOne(serviceInfo);
+        res.json(result);
+        console.log("[*] Service uploaded to database");
+      });
+
       //DELETE API
-      app.delete("/dbuser/:id", async (req, res) => {
+      app.delete("/placeorder/:id", async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
-        const result = await haiku.deleteOne(query);
+        const result = await haiku3.deleteOne(query);
         console.log("deleteing user with id", result);
         res.json(result);
       });
@@ -65,12 +109,6 @@ client.connect((err) => {
     }
   }
   run().catch(console.dir);
-});
-
-// Server Status
-
-app.get("/", (req, res) => {
-  res.status(200).send("Server Running");
 });
 
 // Listing Port
